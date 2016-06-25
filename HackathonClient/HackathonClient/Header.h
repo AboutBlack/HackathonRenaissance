4771290cff2c479d356b793b4425e984fc99eacf
ShaneZhang <zhangyuqing@5288z.com>
2016-06-25 21:16:00 +0800

    调通登录的逻辑

---
+++ HackathonClient/HackathonClient/Header.h
@@ -0,0 +1,14 @@
+//
+//  Header.h
+//  HackathonClient
+//
+//  Created by Yuqing Zhang on 16/6/25.
+//  Copyright © 2016年 AboutBlack. All rights reserved.
+//
+
+#ifndef Header_h
+#define Header_h
+
+#define kUser_Name  @"user_Name"
+
+#endif /* Header_h */
