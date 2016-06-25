2335a407b33c4feadf684bf08a31a7d9b7e1a29a
ShaneZhang <zhangyuqing@5288z.com>
2016-06-25 17:02:19 +0800

    添加七牛的SDK

---
+++ HackathonClient/Pods/Target Support Files/Qiniu/Qiniu-umbrella.h
@@ -0,0 +1,24 @@
+#import <UIKit/UIKit.h>
+
+#import "AFHTTPRequestOperation.h"
+#import "AFHTTPRequestOperationManager.h"
+#import "AFHTTPSessionManager.h"
+#import "AFNetworking.h"
+#import "AFNetworkReachabilityManager.h"
+#import "AFSecurityPolicy.h"
+#import "AFURLConnectionOperation.h"
+#import "AFURLRequestSerialization.h"
+#import "AFURLResponseSerialization.h"
+#import "AFURLSessionManager.h"
+#import "QiniuBlkputRet.h"
+#import "QiniuConfig.h"
+#import "QiniuHttpClient.h"
+#import "QiniuResumableClient.h"
+#import "QiniuResumableUploader.h"
+#import "QiniuSimpleUploader.h"
+#import "QiniuUploadDelegate.h"
+#import "QiniuUtils.h"
+
+FOUNDATION_EXPORT double QiniuVersionNumber;
+FOUNDATION_EXPORT const unsigned char QiniuVersionString[];
+
