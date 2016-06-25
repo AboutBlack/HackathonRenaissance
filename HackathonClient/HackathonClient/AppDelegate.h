3ac1673e83347d1d552a1b2af480e37c05924b4d
talisk <talisk@talisk.cn>
2016-06-25 13:55:04 +0800

    Add client project.

---
+++ HackathonClient/HackathonClient/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HackathonClient
+//
+//  Created by 孙恺 on 16/6/25.
+//  Copyright © 2016年 AboutBlack. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ HackathonClient/HackathonClient/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HackathonClient
+//
+//  Created by 孙恺 on 16/6/25.
+//  Copyright © 2016年 AboutBlack. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
